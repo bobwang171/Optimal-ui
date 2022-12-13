e56ec268040a8d8dae198d8e9722fbfa5f408d63
bobwang171 <103095277+bobwang171@users.noreply.github.com>
2022-12-13 17:47:35 +0800

    正在完成button组件

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Button from './button/Button.vue';
+</script>
+
+<template>
+  <div id="app">
+    <Button class="button" icon="setting">
+     
+     <span>按钮</span> 
+    </Button>
+        
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/button/Button.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="app">
+       <button class="g-button">
+        <svg v-if="icon" class="icon"><use :xlink:href="`#icon-${icon}`"></use></svg>
+        <slot></slot>
+       </button> 
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        props:["icon"]
+    }
+</script>
+
+
+<style lang="scss" scoped>
+*{margin: 0; padding: 0; box-sizing: border-box;}
+:root {
+    --button-height: 32px;
+    --font-size: 14px;
+    --button-bg: #fff;
+    --button-active-bg: #fff;
+    --border-radius: 4px;
+    --color: #333;
+    --border-color: #999;
+    --border-color-hover: #666
+}
+.g-button{
+    height:var(--button-height);
+    font-size: var(--font-size);
+    background: var(--button-bg);
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    padding: 0 1em;
+}
+.icon{
+  height: 1em;
+  width: 1em;
+}
+</style>
