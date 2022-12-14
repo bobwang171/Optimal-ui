4c72bfe211f0740f36c21f7aafceeb3ec064cac4
bobwang171 <103095277+bobwang171@users.noreply.github.com>
2022-12-14 13:58:50 +0800

    完成button组件

--- src/button/button-group.vue
+++ src/button/button-group.vue
@@ -16,7 +16,10 @@ export default {
     >.o-button {
         border-radius: 0;
 
-        margin-left: -1px;
+        &:not(:first-child) {
+            margin-left: -1px;
+        }
+
 
         &:first-child {
             border-top-left-radius: var(--border-radius);
