c38c309278e8e7c62c36604d5fb8dc07b7dd3e98
bobwang171 <103095277+bobwang171@users.noreply.github.com>
2022-12-14 14:04:59 +0800

    优化button组件

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
 export default {
   data() {
     return {
-      loadingStatus: true
+      loadingStatus1: true,
+      loadingStatus2: false,
+      loadingStatus3: false
     }
   },
 }
@@ -11,13 +13,16 @@ export default {
 <template>
   <div id="app" class="app">
 
-    <o-button :loading="loadingStatus" @click="loadingStatus = !loadingStatus" name="setting"
+    <o-button :loading="loadingStatus1" @click="loadingStatus1 = !loadingStatus1" name="setting"
       icon-position="left"><span>按钮</span>
     </o-button>
 
-    <o-button name="setting" icon-position="right"><span>按钮</span> </o-button>
+    <o-button :loading="loadingStatus2" @click="loadingStatus2 = !loadingStatus2" name="setting"
+      icon-position="right"><span>按钮</span> </o-button>
 
-    <o-button name="setting"><span>按钮</span> </o-button>
+    <o-button :loading="loadingStatus3" @click="loadingStatus3 = !loadingStatus3" name="setting"><span>按钮</span>
+    </o-button>
+
     <o-button-group>
       <o-button name="back"><span>上页</span> </o-button>
       <o-button><span>更多</span> </o-button>
