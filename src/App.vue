a42ee9813cfc8c879b4cc34ef808ca8b222ed0ac
bobwang171 <103095277+bobwang171@users.noreply.github.com>
2022-12-23 17:21:00 +0800

    改善样式

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,14 @@ export default {
     }
   },
   methods: {
-    showToast() {
+    showToast1() {
+      this.$toast(`我是toast${Math.floor(Math.random() * 100)}`, { enableHTML: true, position: "top" })
+    },
+    showToast2() {
       this.$toast(`我是toast${Math.floor(Math.random() * 100)}`, { enableHTML: true, position: "middle" })
+    },
+    showToast3() {
+      this.$toast(`我是toast${Math.floor(Math.random() * 100)}`, { enableHTML: true, position: "bottom" })
     }
   }
 }
@@ -18,10 +24,67 @@ export default {
 
 <template>
   <div id="app" class="app">
+    <div class="button_wrapper">
+      <!-- button 按钮展示 -->
+      <o-button :loading="loadingStatus1" @click="loadingStatus1 = !loadingStatus1" name="setting"
+        icon-position="left"><span>按钮</span>
+      </o-button>
 
-    <div>
-      <button @click="showToast"> 点我</button>
+      <o-button :loading="loadingStatus2" @click="loadingStatus2 = !loadingStatus2" name="setting"
+        icon-position="right"><span>按钮</span> </o-button>
+
+      <o-button :loading="loadingStatus3" @click="loadingStatus3 = !loadingStatus3" name="setting"><span>按钮</span>
+      </o-button>
+
+      <o-button :disabled="true"><span>按钮</span>
+      </o-button>
+
+      <o-button-group>
+        <o-button name="back"><span>上页</span> </o-button>
+        <o-button><span>更多</span> </o-button>
+        <o-button name="front" icon-position="right"><span>下页</span> </o-button>
+      </o-button-group>
     </div>
+    <div class="input_wrapper">
+      <!-- Input展示 -->
+      <div class="inputModel">
+        <o-input v-model="massage"></o-input>
+        <span>Value: {{ massage }}</span>
+      </div>
+      <o-input value="Susan" :disabled="true"></o-input>
+      <o-input value="Kevin" :readonly="true"></o-input>
+      <o-input value="Bob" error="错误信息"></o-input>
+    </div>
+    <!-- Toast展示 -->
+    <div class="toast_wrapper">
+      <button @click="showToast1"> 点击查看Toast</button>
+      <button @click="showToast2"> 点击查看Toast</button>
+      <button @click="showToast3"> 点击查看Toast</button>
+    </div>
+    <!-- grid 格栅展示 -->
+    <div class="grid-wrapper">
+      <o-row class="row">
+        <o-col span="24" class="col-item">111</o-col>
+      </o-row>
+      <o-row class="row">
+        <o-col span="8" class="col-item">111</o-col>
+        <o-col span="8" class="col-item">111</o-col>
+        <o-col span="8" class="col-item">111</o-col>
+
+      </o-row>
+    </div>
+
+    <!-- 布局展示 -->
+    <o-layout style="height: 100vh;">
+      <o-header class="demo">header</o-header>
+
+      <o-layout>
+        <o-content class="demo">content</o-content>
+        <o-sider class="demo">sider</o-sider>
+      </o-layout>
+      <o-footer class="demo">footer</o-footer>
+    </o-layout>
+
   </div>
 
 </template>
@@ -33,6 +96,18 @@ export default {
   padding: 0;
   margin: 0;
   box-sizing: border-box;
+
+  .toast_wrapper {
+    display: flex;
+    margin-bottom: 20px;
+    background-color: #fff;
+    border: none;
+
+    >button {
+      margin-right: 8px;
+    }
+  }
+
 
   .demo {
     display: flex;
