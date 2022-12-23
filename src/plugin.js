35e3dac98d13f2df087580a27b2f57407e82d943
bobwang171 <103095277+bobwang171@users.noreply.github.com>
2022-12-23 12:29:48 +0800

    每次只能出现一个toast

--- src/plugin.js
+++ src/plugin.js
@@ -1,12 +1,25 @@
 import Toast from './toast/toast.vue'
+
+let currentToast
+
 export default {
     install(Vue,options) {
-        Vue.prototype.$toast = (message,toastOptions) => {
-            let Constructor = Vue.extend(Toast)
-            let toast = new Constructor()
-            toast.$slots.default = [message]
-            toast.$mount()
-            document.body.appendChild(toast.$el)
+        Vue.prototype.$toast = (message, toastOptions) => {
+            if (currentToast) {
+             currentToast.close()
+         }
+           currentToast = createToast({ Vue, message, propsData: toastOptions })
         }
     }
+}       
+
+function createToast({Vue,message,propsData}) {
+    let Constructor = Vue.extend(Toast) 
+    let toast = new Constructor({
+        propsData
+    })
+    toast.$slots.default = [message]
+    toast.$mount()
+    document.body.appendChild(toast.$el)
+    return toast
 }
