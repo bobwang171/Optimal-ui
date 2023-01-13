db884ddea69454fc8e397396cc7192e62bcb39d1
bobwang171 <103095277+bobwang171@users.noreply.github.com>
2023-01-13 16:04:00 +0800

    更新

--- src/popover/popover.vue
+++ src/popover/popover.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="popover" @click="xxx">
-        <slot name="content" v-if="visible"></slot>
+        <div v-if="visible">
+            <slot name="content"></slot>
+        </div>
         <slot></slot>
     </div>
 </template>
@@ -21,6 +23,7 @@ export default {
 </script>
 <style lang="scss">
 .popover {
-    display: inline-flex;
+    display: flex;
+    flex-direction: column;
 }
 </style>
