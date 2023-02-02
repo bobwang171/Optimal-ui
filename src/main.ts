92cf050dae5d99af38d3faa6b85493f7602af3e1
bobwang171 <103095277+bobwang171@users.noreply.github.com>
2023-02-02 18:24:51 +0800

    删除了Icon.vue

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import Button from './button/button.vue'
-import Icon from './button/icon.vue'
 import ButtonGroup from './button/button-group.vue'
 import Input from './input/input.vue'
 import Row from './grid/row.vue'
@@ -24,7 +23,6 @@ import './style/var.scss'
 
 
 Vue.component("o-button", Button)
-Vue.component("o-icon",Icon)
 Vue.component("o-button-group", ButtonGroup)
 Vue.component("o-input", Input)
 Vue.component("o-row", Row)
